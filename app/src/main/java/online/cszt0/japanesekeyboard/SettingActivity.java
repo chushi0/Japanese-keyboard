847a3e73063666c9810cda71a953d888a556e829
chushi0 <wangyongchao49@sina.com>
2019-04-28 23:40:55 +0800

    initial commit

---
+++ app/src/main/java/online/cszt0/japanesekeyboard/SettingActivity.java
@@ -0,0 +1,13 @@
+package online.cszt0.japanesekeyboard;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SettingActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_setting);
+	}
+}
