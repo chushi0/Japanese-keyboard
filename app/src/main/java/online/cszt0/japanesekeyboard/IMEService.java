b5b24fa373610184607252ba0d05c1889b72e1ac
chushi0 <wangyongchao49@sina.com>
2019-04-30 12:52:21 +0800

    增加多个按钮，增加样式

--- app/src/main/java/online/cszt0/japanesekeyboard/IMEService.java
+++ app/src/main/java/online/cszt0/japanesekeyboard/IMEService.java
@@ -16,6 +16,8 @@ public class IMEService extends InputMethodService {
 
 	private Button[] alphabetKey;
 	private Button deleteButton;
+	private Button spaceButton;
+	private Button enterButton;
 
 	@Override
 	public void onCreate() {
@@ -25,29 +27,25 @@ public class IMEService extends InputMethodService {
 		// findViewsById
 		input = view.findViewById(R.id.input);
 		candidate = view.findViewById(R.id.candidate);
-		candidate.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				String text = candidate.getText().toString();
-				if (!text.isEmpty()) {
-					char[] chars = text.toCharArray();
-					for (char c : chars) {
-						sendKeyChar(c);
-					}
-					input.setText(null);
-					candidate.setText(null);
+		candidate.setOnClickListener(v -> {
+			String text = candidate.getText().toString();
+			if (!text.isEmpty()) {
+				char[] chars = text.toCharArray();
+				for (char c : chars) {
+					sendKeyChar(c);
 				}
+				input.setText(null);
+				candidate.setText(null);
+				enterButton.setText("┘");
 			}
 		});
 		alphabetKey = new Button[26];
-		View.OnClickListener alphabetKeyListener = new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Button button = (Button) v;
-				String text = button.getText().toString().toLowerCase();
-				input.append(text);
-				candidate.setText(Dictionary.parseHiragana(input.getText().toString()));
-			}
+		View.OnClickListener alphabetKeyListener = v -> {
+			Button button = (Button) v;
+			String text = button.getText().toString().toLowerCase();
+			input.append(text);
+			candidate.setText(Dictionary.parseHiragana(input.getText().toString()));
+			enterButton.setText("×");
 		};
 		for (int i = 0; i < 26; i++) {
 			char c = (char) ('a' + i);
@@ -57,21 +55,38 @@ public class IMEService extends InputMethodService {
 			alphabetKey[i].setOnClickListener(alphabetKeyListener);
 		}
 		deleteButton = view.findViewById(R.id.key_del);
-		deleteButton.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				if (input.getText().length() == 0) {
-					sendDownUpKeyEvents(KeyEvent.KEYCODE_DEL);
+		deleteButton.setOnClickListener(v -> {
+			if (input.getText().length() == 0) {
+				sendDownUpKeyEvents(KeyEvent.KEYCODE_DEL);
+			} else {
+				String text = input.getText().toString();
+				text = text.substring(0, text.length() - 1);
+				input.setText(text);
+				if (text.isEmpty()) {
+					candidate.setText(null);
+					enterButton.setText("┘");
 				} else {
-					String text = input.getText().toString();
-					text = text.substring(0, text.length() - 1);
-					input.setText(text);
-					if (text.isEmpty()) {
-						candidate.setText(null);
-					} else {
-						candidate.setText(Dictionary.parseHiragana(text));
-					}
+					candidate.setText(Dictionary.parseHiragana(text));
 				}
+			}
+		});
+		spaceButton = view.findViewById(R.id.key_space);
+		spaceButton.setOnClickListener(v -> {
+			String text = candidate.getText().toString();
+			if (text.isEmpty()) {
+				sendKeyChar(' ');
+			} else {
+				candidate.performClick();
+			}
+		});
+		enterButton = view.findViewById(R.id.key_enter);
+		enterButton.setOnClickListener(v -> {
+			if (input.getText().length() == 0) {
+				sendDownUpKeyEvents(KeyEvent.KEYCODE_ENTER);
+			} else {
+				input.setText(null);
+				candidate.setText(null);
+				enterButton.setText("┘");
 			}
 		});
 
